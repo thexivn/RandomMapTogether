b7456f69cdb46bd8b57159857eccce60060c2623
thexivn <thexivn@proton.me>
2022-11-19 13:31:44 +0100

    removed storage

--- it/thexivn/random_maps_together/MapHandler.py
+++ it/thexivn/random_maps_together/MapHandler.py
@@ -4,7 +4,7 @@ import logging
 from pyplanet.apps.core.maniaplanet.models import Map
 from pyplanet.contrib.map import MapManager
 from pyplanet.contrib.map.exceptions import MapException
-from pyplanet.core.storage.storage import Storage
+
 
 from it.thexivn.random_maps_together.RestClient.TMNXRestClient import TMNXRestClient
 
@@ -12,22 +12,22 @@ logger = logging.getLogger(__name__)
 
 
 class MapHandler:
-    def __init__(self, map_manager: MapManager, storage: Storage):
+    def __init__(self, map_manager: MapManager):
         self.tmnx_rest_client = TMNXRestClient()
         self.hub_map = '7E1heauBgOUsqlhliGDY8DoOZbm'
         self.hub_id = '63710'
         self.map_manager = map_manager
         self.loaded_map = None
         self.event_map: Map = None
-        self._sotorage = storage
 
     async def load_next_map(self):
         logger.info("Trying to load next map ...")
         self.loaded_map = self.tmnx_rest_client.get_random_map()
         map_to_remove = self.map_manager.current_map
-        logger.info(f'{self.loaded_map.get("uuid")}.Map.Gbx')
-        await self.upload_map_new(io.BytesIO(self.loaded_map.get("content")),
-                                          f'{self.loaded_map.get("uuid")}.Map.Gbx', overwrite=True)
+        uuid = self.loaded_map.get("uuid")
+        logger.info(f'{uuid}.Map.Gbx')
+        await self.map_manager.upload_map(io.BytesIO(self.loaded_map.get("content")),
+                                          f'{uuid}.Map.Gbx', overwrite=True)
         await self.map_manager.update_list(True, True)
         logger.info("uploaded next map...")
         await self.map_manager.set_current_map(self.loaded_map.get("uuid"))
@@ -38,7 +38,7 @@ class MapHandler:
         logger.info("loading HUB map ...")
         content = self.tmnx_rest_client.map_map_content(self.hub_id)
         try:
-            await self.upload_map_new(io.BytesIO(content), f'{self.hub_id}.Map.Gbx', overwrite=True)
+            await self.map_manager.upload_map(io.BytesIO(content), f'{self.hub_id}.Map.Gbx', overwrite=True)
             await self.map_manager.update_list(True, True)
             await self.map_manager.set_current_map(self.hub_map)
         except:
@@ -69,30 +69,3 @@ class MapHandler:
         if self.event_map:
             return self.event_map.time_silver
         return 0
-
-
-    async def upload_map_new(self, fh, filename, insert=True, overwrite=False):
-        """
-        Upload and add/insert the map to the current online playlist.
-
-        :param fh: File handler, bytesio object or any readable context.
-        :param filename: The filename when saving on the server. Must include the map.gbx! Relative to 'Maps' folder.
-        :param insert: Insert after the current map, this will make it play directly after the current map. True by default.
-        :param overwrite: Overwrite current file if exists? Default False.
-        :type filename: str
-        :type insert: bool
-        :type overwrite: bool
-        :raise: pyplanet.contrib.map.exceptions.MapIncompatible
-        :raise: pyplanet.contrib.map.exceptions.MapException
-        :raise: pyplanet.core.storage.exceptions.StorageException
-        """
-        exists = await self._sotorage.driver.exists(filename)
-        if exists and not overwrite:
-            raise MapException('Map with filename already located on server!')
-        if not exists:
-            await self._sotorage.driver.touch('{}/{}'.format(self._instance.storage.MAP_FOLDER, filename))
-
-        async with self._sotorage.open_map(filename, 'wb+') as fw:
-            await fw.write(fh.read(-1))
-
-        return await self.map_manager.add_map(filename, insert=insert)
